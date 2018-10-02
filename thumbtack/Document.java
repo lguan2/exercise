bd5e0a6e24f049505c6afea1b15c0ff889b53b0f
Lin Guan <lguan0809@gmail.com>
2018-10-01 23:39:21 -0700

    add tf-idf solution

---
+++ thumbtack/Document.java
@@ -0,0 +1,16 @@
+package algorithm.thumbtack;
+
+import java.util.List;
+
+/**
+ * Created by linguan on 10/1/18.
+ */
+public class Document {
+    String id;
+    List<String> words;
+
+    public Document(){};
+    public Document(String id){
+        this.id = id;
+    }
+}
